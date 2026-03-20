331e98e15e4ce00fe4240cf18f195564a29f38e6
Lindsay Bernath <78574116+lybrn@users.noreply.github.com>
2026-03-20 18:01:14 +0000

    updates drupal install script

--- drupal-up.bash
+++ drupal-up.bash
@@ -2,14 +2,20 @@
 sudo apt-get update
 sudo apt-get install -y php8.3-gd
 ln -s /usr/lib/php/20230831/gd.so /usr/local/php/8.3.14/extensions/
+ln -s /usr/share/php8.3-gd/gd/gd.ini /usr/local/php/8.3.14/ini/conf.d/
 php -m | grep gd
 
-# install drupal qith sqllite
+# install drupal qith sqlliteps
 composer create-project drupal/recommended-project:^10 drupal-test 
 cd drupal-test
 composer require drush/drush 
 vendor/bin/drush site:install  --db-url=sqlite://sites/default/files/db.sqlite -y
 
+# configure php
+vendor/bin/drush theme:dev on
+vendor/bin/drush user-password admin --password="dru-dev-admin"
+
 # start web server
 php -S 0.0.0.0:8080 -t web
+
 
